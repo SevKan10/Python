94b55abb2e77a8295c08a6ed79df914cb48c6995
KKhang <khangkhuu10727@gmail.com>
2023-07-19 22:58:34 +0700

    add comment

--- Assistant/assis.py
+++ Assistant/assis.py
@@ -5,8 +5,7 @@ import pytz
 
 engine = pyttsx3.init()
 rate = engine.getProperty('rate')
-engine.setProperty('rate', rate - 50)  # Adjust the rate value as needed
-
+engine.setProperty('rate', rate - 60)  # làm tốc độ nói chậm lại
 robotEar = sr.Recognizer()
 today = date.today()
 now = datetime.now()
@@ -27,10 +26,12 @@ while True:
     print("You: " + you)
     if you == "":
         robotSay = "I can't understand you"
-    elif "hello" in you or "hi" in you or "hey javist" in you:
+    elif "hello" in you or "hi" in you or "hey seven" in you:
         robotSay = "Hello Khang"
+    elif "my girlfriend" in you:
+        robotSay = "What do you do with a lover?"
     elif "your name" in you:
-        robotSay = "My name's Javist"
+        robotSay = "My name's Seven. I'm a virtual assistant created by Khang"
     elif "today" in you:
         robotSay = today.strftime("%B %d, %Y")
     elif "time" in you:
@@ -38,7 +39,7 @@ while True:
     elif "time zone in Vietnam" in you:
         datetime_VN = datetime.now(tz_VN)
         robotSay = datetime_VN.strftime("%m/%d/%Y, %H:%M:%S")
-    elif "bye" in you:
+    elif "bye" in you or "stop" in you:
         robotSay = "Goodbye sir"
         print(robotSay)
         engine.say(robotSay)
